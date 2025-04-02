5b6025c86ab08673e1f59dac35faae9339fb856b
Osorio-Sanchez-24580160 <omarosorio553@gmail.com>
2025-04-02 16:34:15 -0500

    Add files via upload

---
+++ Practica 15/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Practica_15
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(24, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(382, 96);
+            label1.TabIndex = 0;
+            label1.Text = "Dado como dato el sueldo de un trabajador,\r\nel programa aplica un aumento de 15% si el\r\nsueldo es menor de 400,000 y si es mayor,\r\nse aumenta 8%.";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Calibri", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(24, 175);
+            label2.Name = "label2";
+            label2.Size = new Size(161, 24);
+            label2.TabIndex = 1;
+            label2.Text = "Ingrese su sueldo:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Calibri", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(49, 305);
+            label3.Name = "label3";
+            label3.Size = new Size(136, 24);
+            label3.TabIndex = 2;
+            label3.Text = "Nuevo sueldo: ";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Calibri", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(52, 239);
+            button1.Name = "button1";
+            button1.Size = new Size(122, 32);
+            button1.TabIndex = 3;
+            button1.Text = "Decisión";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Calibri", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(191, 171);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(174, 32);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Calibri", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(191, 302);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(174, 32);
+            textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Calibri", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(215, 239);
+            button2.Name = "button2";
+            button2.Size = new Size(122, 32);
+            button2.TabIndex = 6;
+            button2.Text = "Borrar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(429, 418);
+            Controls.Add(button2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = " Práctica 15";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button2;
+    }
+}
---
+++ Practica 16/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Practica_16
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(56, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(395, 105);
+            label1.TabIndex = 0;
+            label1.Text = "Dado como un dato un número,\r\nel progama determina si es par o\r\nimpar y divisible entre 3";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Calibri", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(30, 226);
+            label2.Name = "label2";
+            label2.Size = new Size(241, 35);
+            label2.TabIndex = 1;
+            label2.Text = "Ingresa un número:";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(277, 229);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(169, 34);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkOrange;
+            button1.Font = new Font("Calibri", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(45, 329);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 53);
+            button1.TabIndex = 3;
+            button1.Text = "Determinar";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DarkOrange;
+            button2.Font = new Font("Calibri", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(251, 329);
+            button2.Name = "button2";
+            button2.Size = new Size(200, 53);
+            button2.TabIndex = 4;
+            button2.Text = "Borrar";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.BurlyWood;
+            ClientSize = new Size(502, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = " Práctica 16";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
