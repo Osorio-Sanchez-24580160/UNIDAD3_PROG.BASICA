6595c1b06166085862e593ab146c7bf2c596482f
Osorio-Sanchez-24580160 <omarosorio553@gmail.com>
2025-04-02 16:31:40 -0500

    Add files via upload

---
+++ Practica 4.  Calcular propiedades del cilindro/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Cilindro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(34, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(586, 29);
+            label1.TabIndex = 0;
+            label1.Text = "INTRODUCE LAS DIMENSIONES DEL CILINDRO";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.cilindro;
+            pictureBox1.Location = new Point(478, 88);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(142, 150);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("SolidWorks GDT", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(93, 112);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Radio";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("SolidWorks GDT", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(87, 165);
+            label3.Name = "label3";
+            label3.Size = new Size(85, 25);
+            label3.TabIndex = 3;
+            label3.Text = "Altura";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("SolidWorks GDT", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(93, 342);
+            label4.Name = "label4";
+            label4.Size = new Size(132, 25);
+            label4.TabIndex = 4;
+            label4.Text = "Base Area";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("SolidWorks GDT", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(66, 398);
+            label5.Name = "label5";
+            label5.Size = new Size(159, 25);
+            label5.TabIndex = 5;
+            label5.Text = "Lateral Area";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("SolidWorks GDT", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(94, 452);
+            label6.Name = "label6";
+            label6.Size = new Size(131, 25);
+            label6.TabIndex = 6;
+            label6.Text = "Total Area";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("SolidWorks GDT", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(111, 507);
+            label7.Name = "label7";
+            label7.Size = new Size(114, 25);
+            label7.TabIndex = 7;
+            label7.Text = "Volumen";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("SolidWorks GDT", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(235, 109);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 34);
+            textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("SolidWorks GDT", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(235, 162);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 34);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("SolidWorks GDT", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(235, 333);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(150, 34);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("SolidWorks GDT", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(235, 389);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(150, 34);
+            textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("SolidWorks GDT", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox5.Location = new Point(235, 443);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(150, 34);
+            textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("SolidWorks GDT", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox6.Location = new Point(235, 504);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(150, 34);
+            textBox6.TabIndex = 13;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("SolidWorks GDT", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button1.Location = new Point(53, 260);
+            button1.Name = "button1";
+            button1.Size = new Size(138, 34);
+            button1.TabIndex = 14;
+            button1.Text = "CALCULAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("SolidWorks GDT", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button2.Location = new Point(212, 260);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 15;
+            button2.Text = "BORRAR";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("SolidWorks GDT", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button3.Location = new Point(348, 259);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 16;
+            button3.Text = "SALIR";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(665, 573);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Calcular propiedades del cilindro";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ Practica 5. Calcular área del triangulo/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Area_Triangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(134, 53);
+            label1.Name = "label1";
+            label1.Size = new Size(233, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Área de Triangulo";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(85, 132);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Base";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(63, 179);
+            label3.Name = "label3";
+            label3.Size = new Size(91, 28);
+            label3.TabIndex = 2;
+            label3.Text = "Altura";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Century", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(229, 129);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 36);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Century", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(229, 176);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 36);
+            textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(85, 255);
+            label4.Name = "label4";
+            label4.Size = new Size(69, 28);
+            label4.TabIndex = 5;
+            label4.Text = "Área";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Century", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(229, 252);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(150, 36);
+            textBox3.TabIndex = 6;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Azure;
+            button1.Font = new Font("Century", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(152, 330);
+            button1.Name = "button1";
+            button1.Size = new Size(168, 34);
+            button1.TabIndex = 7;
+            button1.Text = "CALCULAR";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Lavender;
+            ClientSize = new Size(485, 394);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Calcular área de triangulo";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label4;
+        private TextBox textBox3;
+        private Button button1;
+    }
+}
