23d5d44ec1e51cd9bda00933e01470fe75e33839
Osorio-Sanchez-24580160 <omarosorio553@gmail.com>
2025-04-02 16:30:56 -0500

    Add files via upload

---
+++ Practica 2. Invierte números/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace _3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("SolidWorks GDT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(126, 73);
+            label1.Name = "label1";
+            label1.Size = new Size(251, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Escribe un número";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("SolidWorks GDT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(126, 123);
+            label2.Name = "label2";
+            label2.Size = new Size(251, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Escribe un número";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("SolidWorks GDT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(126, 171);
+            label3.Name = "label3";
+            label3.Size = new Size(251, 28);
+            label3.TabIndex = 2;
+            label3.Text = "Escribe un número";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("SolidWorks GDT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(126, 219);
+            label4.Name = "label4";
+            label4.Size = new Size(251, 28);
+            label4.TabIndex = 3;
+            label4.Text = "Escribe un número";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("SolidWorks GDT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(237, 346);
+            label5.Name = "label5";
+            label5.Size = new Size(46, 28);
+            label5.TabIndex = 4;
+            label5.Text = "N1";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("SolidWorks GDT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(326, 346);
+            label6.Name = "label6";
+            label6.Size = new Size(46, 28);
+            label6.TabIndex = 5;
+            label6.Text = "N2";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("SolidWorks GDT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(420, 346);
+            label7.Name = "label7";
+            label7.Size = new Size(46, 28);
+            label7.TabIndex = 6;
+            label7.Text = "N3";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("SolidWorks GDT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(511, 346);
+            label8.Name = "label8";
+            label8.Size = new Size(46, 28);
+            label8.TabIndex = 7;
+            label8.Text = "N4";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("SolidWorks GDT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(604, 219);
+            button1.Name = "button1";
+            button1.Size = new Size(134, 34);
+            button1.TabIndex = 8;
+            button1.Text = "Invertir";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.Font = new Font("SolidWorks GDT", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button2.Location = new Point(604, 276);
+            button2.Name = "button2";
+            button2.Size = new Size(134, 34);
+            button2.TabIndex = 9;
+            button2.Text = "SALIR";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("SolidWorks GDT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(407, 69);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 36);
+            textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("SolidWorks GDT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(407, 120);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 36);
+            textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("SolidWorks GDT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(407, 168);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(150, 36);
+            textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("SolidWorks GDT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(407, 216);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(150, 36);
+            textBox4.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Linen;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Invertir Número";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+    }
+}
