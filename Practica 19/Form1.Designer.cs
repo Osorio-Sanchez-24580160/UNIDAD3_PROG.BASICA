7ebcfa7062e54b01312ef9720b465dca2dab08db
Osorio-Sanchez-24580160 <omarosorio553@gmail.com>
2025-04-02 16:36:07 -0500

    Add files via upload

---
+++ Practica 19/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Practica_19
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label6 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(70, 107);
+            label1.Name = "label1";
+            label1.Size = new Size(272, 27);
+            label1.TabIndex = 0;
+            label1.Text = "Ingresa la primer calificacion";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(55, 161);
+            label2.Name = "label2";
+            label2.Size = new Size(287, 27);
+            label2.TabIndex = 1;
+            label2.Text = "Ingresa la segunda calificacion";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(77, 214);
+            label3.Name = "label3";
+            label3.Size = new Size(265, 27);
+            label3.TabIndex = 2;
+            label3.Text = "Ingresa la tercer calificacion";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(73, 267);
+            label4.Name = "label4";
+            label4.Size = new Size(269, 27);
+            label4.TabIndex = 3;
+            label4.Text = "Ingresa la cuarta calificacion";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(359, 104);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(214, 34);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(359, 161);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(214, 34);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(359, 214);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(214, 34);
+            textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(359, 264);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(214, 34);
+            textBox4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(96, 449);
+            label5.Name = "label5";
+            label5.Size = new Size(146, 27);
+            label5.TabIndex = 6;
+            label5.Text = "Decisión FINAL";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox5.Location = new Point(248, 446);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(214, 34);
+            textBox5.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Peru;
+            button1.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(80, 348);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 35);
+            button1.TabIndex = 8;
+            button1.Text = "Decisión";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Peru;
+            button2.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(259, 348);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 35);
+            button2.TabIndex = 9;
+            button2.Text = "Borrar";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Peru;
+            button3.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(434, 348);
+            button3.Name = "button3";
+            button3.Size = new Size(150, 35);
+            button3.TabIndex = 9;
+            button3.Text = "Salir";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Calibri", 11F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label6.Location = new Point(122, 41);
+            label6.Name = "label6";
+            label6.Size = new Size(421, 27);
+            label6.TabIndex = 10;
+            label6.Text = "Este programa dice si apruebas o repurebas";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.cal_removebg_preview;
+            pictureBox1.Location = new Point(616, 64);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(257, 249);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 11;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Bisque;
+            ClientSize = new Size(894, 605);
+            Controls.Add(pictureBox1);
+            Controls.Add(label6);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Practica 19";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label6;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Practica 20/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Practica_20
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(87, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(507, 54);
+            label1.TabIndex = 0;
+            label1.Text = "Introduce tu edad y el programa determina si eres un \r\nniño, un adolescente, un adulto o un adulto mayor.";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(34, 149);
+            label2.Name = "label2";
+            label2.Size = new Size(184, 27);
+            label2.TabIndex = 1;
+            label2.Text = "Introduce tu edad:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(66, 337);
+            label3.Name = "label3";
+            label3.Size = new Size(95, 27);
+            label3.TabIndex = 2;
+            label3.Text = "Decisión:";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(232, 145);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(95, 34);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(167, 334);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(214, 34);
+            textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(67, 204);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 42);
+            button1.TabIndex = 4;
+            button1.Text = "Decisión";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(200, 204);
+            button2.Name = "button2";
+            button2.Size = new Size(127, 42);
+            button2.TabIndex = 4;
+            button2.Text = "Borrar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Calibri", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(133, 263);
+            button3.Name = "button3";
+            button3.Size = new Size(127, 42);
+            button3.TabIndex = 4;
+            button3.Text = "Salir";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.edad;
+            pictureBox1.Location = new Point(396, 112);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(215, 211);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Khaki;
+            ClientSize = new Size(678, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Practica 20";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private PictureBox pictureBox1;
+    }
+}
