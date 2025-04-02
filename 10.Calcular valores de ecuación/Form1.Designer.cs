9d3963a618bebf7cd6bc8ae07be5d8e4902e0697
Osorio-Sanchez-24580160 <omarosorio553@gmail.com>
2025-04-02 16:29:23 -0500

    Add files via upload

---
+++ 10.Calcular valores de ecuación/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Calcular_valores_de_ecuación
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            listBox4 = new ListBox();
+            button1 = new Button();
+            label8 = new Label();
+            label9 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Verdana", 16F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(247, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(167, 38);
+            label1.TabIndex = 0;
+            label1.Text = "z= x + x";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Verdana", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(332, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(23, 22);
+            label2.TabIndex = 1;
+            label2.Text = "2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Verdana", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Location = new Point(405, 43);
+            label3.Name = "label3";
+            label3.Size = new Size(23, 22);
+            label3.TabIndex = 2;
+            label3.Text = "3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Verdana", 16F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label4.Location = new Point(84, 118);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 38);
+            label4.TabIndex = 3;
+            label4.Text = "x";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Verdana", 16F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label5.Location = new Point(236, 118);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 38);
+            label5.TabIndex = 4;
+            label5.Text = "x";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Verdana", 16F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label6.Location = new Point(386, 118);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 38);
+            label6.TabIndex = 5;
+            label6.Text = "x";
+            label6.Click += label6_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Verdana", 16F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label7.Location = new Point(532, 118);
+            label7.Name = "label7";
+            label7.Size = new Size(36, 38);
+            label7.TabIndex = 6;
+            label7.Text = "z";
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.PaleTurquoise;
+            listBox1.Font = new Font("Verdana", 9F, FontStyle.Bold);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 22;
+            listBox1.Location = new Point(50, 168);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(116, 180);
+            listBox1.TabIndex = 7;
+            // 
+            // listBox2
+            // 
+            listBox2.BackColor = Color.PaleTurquoise;
+            listBox2.Font = new Font("Verdana", 9F, FontStyle.Bold);
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 22;
+            listBox2.Location = new Point(195, 168);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(116, 180);
+            listBox2.TabIndex = 8;
+            // 
+            // listBox3
+            // 
+            listBox3.BackColor = Color.PaleTurquoise;
+            listBox3.Font = new Font("Verdana", 9F, FontStyle.Bold);
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 22;
+            listBox3.Location = new Point(345, 168);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(116, 180);
+            listBox3.TabIndex = 9;
+            // 
+            // listBox4
+            // 
+            listBox4.BackColor = Color.PaleTurquoise;
+            listBox4.Font = new Font("Verdana", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            listBox4.FormattingEnabled = true;
+            listBox4.ItemHeight = 22;
+            listBox4.Location = new Point(488, 168);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(116, 180);
+            listBox4.TabIndex = 10;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.PaleTurquoise;
+            button1.Font = new Font("Verdana", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(271, 363);
+            button1.Name = "button1";
+            button1.Size = new Size(123, 35);
+            button1.TabIndex = 11;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Verdana", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label8.Location = new Point(264, 110);
+            label8.Name = "label8";
+            label8.Size = new Size(23, 22);
+            label8.TabIndex = 12;
+            label8.Text = "2";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Verdana", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label9.Location = new Point(416, 109);
+            label9.Name = "label9";
+            label9.Size = new Size(23, 22);
+            label9.TabIndex = 13;
+            label9.Text = "3";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkCyan;
+            ClientSize = new Size(668, 450);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(button1);
+            Controls.Add(listBox4);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Calcular ecuacion ";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private ListBox listBox4;
+        private Button button1;
+        private Label label8;
+        private Label label9;
+    }
+}
---
+++ 9.factorial de un número/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace factorial_de_un_número
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            textBox1 = new TextBox();
+            listBox1 = new ListBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Verdana", 11F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(83, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(303, 26);
+            label1.TabIndex = 0;
+            label1.Text = "Factorial de un número";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Verdana", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(404, 49);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(127, 34);
+            textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(281, 107);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(250, 179);
+            listBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Verdana", 11F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(15, 336);
+            label2.Name = "label2";
+            label2.Size = new Size(271, 26);
+            label2.TabIndex = 3;
+            label2.Text = "Factorial del número";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Verdana", 11F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Location = new Point(425, 336);
+            label3.Name = "label3";
+            label3.Size = new Size(40, 26);
+            label3.TabIndex = 4;
+            label3.Text = "es";
+            label3.Click += label3_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Verdana", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(292, 333);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(127, 34);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Verdana", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(471, 336);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(248, 29);
+            textBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkOliveGreen;
+            button1.Font = new Font("Verdana", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(564, 107);
+            button1.Name = "button1";
+            button1.Size = new Size(133, 38);
+            button1.TabIndex = 7;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DarkOliveGreen;
+            button2.Font = new Font("Verdana", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(564, 175);
+            button2.Name = "button2";
+            button2.Size = new Size(133, 38);
+            button2.TabIndex = 8;
+            button2.Text = "Borrar";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkSeaGreen;
+            ClientSize = new Size(750, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(listBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "  Factorial de un número";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Button button2;
+    }
+}
